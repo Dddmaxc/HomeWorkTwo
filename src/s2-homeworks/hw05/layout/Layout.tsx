1fad4f55a835beee8c902dc3e80aece578d13c03
Denys Tanov(Itachi) Dddmaxc <navigremory.666@gmail.com>
2024-04-05 20:45:07 +0200

    HomeWork 5

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -3,27 +3,28 @@ import { Header } from '../header/Header'
 import { Sidebar } from '../sidebar/Sidebar'
 
 type PropsType = {
-    children: ReactNode
+	children: ReactNode
 }
 
 export const Layout: FC<PropsType> = ({ children }) => {
-    const [open, setOpen] = useState(false)
-    const handleClose = () => setOpen(false)
-    const handleOpen = () => setOpen(true)
+	const [open, setOpen] = useState(false)
+	const handleClose = () => setOpen(false)
+	const handleOpen = () => setOpen(true)
 
-    useEffect(() => {
-        open && (document.body.style.overflow = 'hidden')
-        !open && (document.body.style.overflow = 'unset')
-    }, [open]) // отключает прокрутку при открытом меню
+	useEffect(() => {
+		open && (document.body.style.overflow = 'hidden')
+		!open && (document.body.style.overflow = 'unset')
+		open && (document.body.style.background = '#0000004c')
+	}, [open]) // отключает прокрутку при открытом меню
 
-    return (
-        <>
-            <Sidebar open={open} handleClose={handleClose} />
-            <Header handleOpen={handleOpen} />
-            <div>
-                {/*страницы*/}
-                {children}
-            </div>
-        </>
-    )
+	return (
+		<>
+			<Header handleOpen={handleOpen} />
+			<Sidebar open={open} handleClose={handleClose} />
+			<div>
+				{/*страницы*/}
+				{children}
+			</div>
+		</>
+	)
 }

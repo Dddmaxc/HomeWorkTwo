6cb89b33c79ccdf32caa7a46f62c911463ca7a8b
Denys Tanov(Itachi) Dddmaxc <navigremory.666@gmail.com>
2024-04-29 16:11:08 +0200

    fdfsdfsdf

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -5,12 +5,6 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
-/*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
- * 3 - сделать стили в соответствии с дизайном
- */
-
 const HW6 = () => {
 	const [value, setValue] = useState<string>('')
 
@@ -19,34 +13,32 @@ const HW6 = () => {
 	}
 	const restore = () => {
 		// делают студенты
-
-		setValue(value)
+		setValue(restoreState<string>('hw6-editable-span-value', ''))
 	}
 
 	return (
 		<div id={'hw6'}>
 			<div className={s2.hwTitle}>Homework #6</div>
 
-			{/*демонстрация возможностей компоненты:*/}
+			{/*should work (должно работать)*/}
 			<div className={s2.hw}>
-				<div className={s.editableSpanContainer}>
+				<div>
 					<SuperEditableSpan
 						id={'hw6-spanable-input'}
 						value={value}
 						onChangeText={setValue}
 						spanProps={{
+							children: value ? undefined : 'enter text...',
 							id: 'hw6-editable-span',
-							defaultText: 'enter text...',
 						}}
 					/>
 				</div>
-
 				<div className={s.buttonsContainer}>
 					<SuperButton id={'hw6-save'} onClick={save}>
-						Save to ls
+						save to ls
 					</SuperButton>
 					<SuperButton id={'hw6-restore'} onClick={restore} xType={'secondary'}>
-						Get from ls
+						get from ls
 					</SuperButton>
 				</div>
 			</div>

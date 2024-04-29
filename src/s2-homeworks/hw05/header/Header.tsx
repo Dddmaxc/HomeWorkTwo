1bdb3d7c89840853faabc175c367fb78e0489a0c
Denys Tanov(Itachi) Dddmaxc <navigremory.666@gmail.com>
2024-04-29 15:50:44 +0200

    'commit'

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -13,14 +13,14 @@ export const Header: FC<PropsType> = ({ handleOpen }) => {
 	const location = useLocation()
 	const currentPath = location.pathname
 
-	const pageName =
-		currentPath === PATH.PRE_JUNIOR
-			? 'Pre-junior'
-			: currentPath === PATH.JUNIOR
-			? 'Junior'
-			: currentPath === PATH.JUNIOR_PLUS
-			? 'Junior Plus'
-			: 'Error'
+	// const pageName =
+	// 	currentPath === PATH.PRE_JUNIOR
+	// 		? 'Pre-junior'
+	// 		: currentPath === PATH.JUNIOR
+	// 		? 'Junior'
+	// 		: currentPath === PATH.JUNIOR_PLUS
+	// 		? 'Junior Plus'
+	// 		: 'Error'
 	return (
 		<>
 			<div id={'hw5-header'} className={s.header}>
@@ -31,7 +31,7 @@ export const Header: FC<PropsType> = ({ handleOpen }) => {
 					onClick={handleOpen}
 					alt={'open menu'}
 				/>
-				<h1>{pageName}</h1>
+				{/* <h1>{pageName}</h1> */}
 			</div>
 		</>
 	)

42892b578040cc935aed39fb1b1796dc94227af5
Denys Tanov(Itachi) Dddmaxc <navigremory.666@gmail.com>
2024-06-26 10:28:17 +0200

    fdfsdfsdf

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,30 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case 'sort': {
+			const sortedState = [...state].sort((a, b) => {
+				if (a.name > b.name) return action.payload === 'up' ? 1 : -1
+				if (a.name < b.name) return action.payload === 'up' ? -1 : 1
+				return 0
+			})
+			// by name
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+			return sortedState // need to fix
+		}
+		case 'check': {
+			let copyState = state.filter(user => user.age >= action.payload) // need to fix
+			return copyState
+		}
+		default:
+			return state
+	}
 }

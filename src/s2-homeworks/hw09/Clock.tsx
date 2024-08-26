eeafb575929330d1e7c25b3408e12eeb06143b87
Denys Tanov(Itachi) Dddmaxc <navigremory.666@gmail.com>
2024-08-26 17:01:37 +0200

    fdfsdfsdf

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -39,8 +39,18 @@ function Clock() {
 		// пишут студенты // спрятать дату если мышка не наведена
 	}
 
-	const stringTime = date.toLocaleTimeString() // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-	const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+	const stringTime = date.toLocaleTimeString('en-US', {
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		hour12: false,
+	}) || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+
+	const stringDate = new Intl.DateTimeFormat('en-US', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+	}).format(date) // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 	// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 	const stringDay = date.toLocaleDateString('en-US', { weekday: 'long' }) // пишут студенты

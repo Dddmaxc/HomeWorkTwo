6ba00afba7ab3c834eed4397e1ff6357e88496fb
Itachi <navigremory.666@gmail.com>
2025-09-07 16:21:31 +0200

    10dz

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
+import { useSelector } from 'react-redux'
 import s from './Loader.module.css'
+import { AppStoreType } from './bll/store'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+ const loading = useSelector((state: AppStoreType) => state.loading)
+ return <>
+ {loading && <div className={s.loader}/>}
+ </>
+}
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,29 @@
-const initState = {
-    isLoading: false,
-}
-
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
-}
-
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
+
+type initStateType = {
+  isLoading: boolean;
+};
+const initState: initStateType = {
+  isLoading: false,
+};
+
+type ActionType = LoadingActionType;
+
+export const loadingReducer = (state = initState, action: ActionType): initStateType => {
+  switch (action.type) {
+    case "CHANGE_LOADING": {
+      return { ...state, isLoading: action.isLoading };
+    }
+    // пишет студент  // need to fix
+    default:
+      return state;
+  }
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: "CHANGE_LOADING",
+  isLoading,
+});
